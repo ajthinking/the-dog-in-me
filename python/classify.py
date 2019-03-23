6d6d3bb5fe9f55d0c180d3bd9ae600837bc5d951
Anders Jurisoo <jurisoo@hotmail.com>
2019-03-23 16:04:40 +0800

    Added the fastai script

--- python/classify.py
+++ python/classify.py
@@ -1,1 +1,11 @@
-print("German Shepard")
+from fastai.vision import *
+
+defaults.device = torch.device('cpu')
+
+learn = load_learner('.')
+
+anders = open_image('./anders.jpg')
+
+pred_class,pred_idx,outputs = learn.predict(anders)
+
+print("This guy looks like", pred_class)
