c364d4bbc347db39c527de33102313ece6f93424
Anders Jurisoo <jurisoo@hotmail.com>
2019-03-24 16:31:57 +0800

    Test with image

--- python/classify.py
+++ python/classify.py
@@ -3,6 +3,7 @@ import sys
 import json
 
 from fastai.vision import *
+import requests
 
 avatar = sys.argv[1]
 
@@ -12,7 +13,11 @@ defaults.device = torch.device('cpu')
 
 learn = load_learner(dir_path)
 
-anders = open_image(dir_path + '/anders.jpg')
+img_data = requests.get(avatar).content
+with open(dir_path + '/temp_image.jpeg', 'wb') as handler:
+    handler.write(img_data)
+
+temp_image = open_image(dir_path + '/temp_image.jpeg')
 
 pred_class,pred_idx,outputs = learn.predict(anders)
 
@@ -20,5 +25,7 @@ result = {}
 result['pred_class'] = str(pred_class)
 result['avatar'] = avatar
 result['accuracy'] = "{0:.0%}".format(outputs.max().item()) 
+
+os.remove(dir_path + '/temp_image.jpeg')
 
 print(json.dumps(result))
